11e638dc1f4c5f7e1d219d62c106c26d3eaa4ee5
user <user@ubuntudev.myguest.virtualbox.org>
2023-07-02 12:58:57 +0300

    make first migration for sqllite

---
+++ db/alembic/versions/bc789024d3a9_create_base_structure.py
@@ -0,0 +1,24 @@
+"""create base structure
+
+Revision ID: bc789024d3a9
+Revises: 
+Create Date: 2023-07-02 12:58:03.874060
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bc789024d3a9'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
