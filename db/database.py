110346f35f99190d8c12638e192f6bbde6d79e3c
user <user@ubuntudev.myguest.virtualbox.org>
2023-07-02 12:47:40 +0300

    make config for db

--- db/database.py
+++ db/database.py
@@ -1,10 +1,12 @@
 from sqlmodel import create_engine, SQLModel
 
+
 from models.users import User, Passwd, Users_roles
 from models.courses import Lesson, Study_lesson, Course
 from models.courses import Courses_context, Study_course
-
-engine = create_engine('sqlite:///./data.db', echo=True)
+from configs.helpers import init_db
+db_conf = init_db()
+engine = create_engine(db_conf['DATABASE_URL'], echo=db_conf['DEBUG_MOD'])
 
 def create_tables():
     SQLModel.metadata.create_all(engine)
