a4e7d341dd458d0fa5e467c81da95f0175485776
user <user@ubuntudev.myguest.virtualbox.org>
2023-07-10 16:57:36 +0300

    try to make offline docs

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
+from .routers.docs_page import doc_router as doc_router
 from .routers.healthcheck import router as router
 
-app = FastAPI()
+app = FastAPI(docs_url=None, redoc_url=None)
 app.include_router(router)
+app.include_router(doc_router)
