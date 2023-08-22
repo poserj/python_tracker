079529ca9a410f9e7b4aec273495aa63c32b71da
user <user@ubuntudev.myguest.virtualbox.org>
2023-08-22 18:20:13 +0300

    add logging+

--- app/logger_project.py
+++ app/logger_project.py
@@ -5,9 +5,12 @@ from app.helpers import get_app_config
 
 def init_logger():
     conf = get_app_config()
+
     logging.basicConfig(
         level=logging.INFO,
         filename=conf["LOG_FILE"],
         filemode="w",
         format="%(asctime)s %(levelname)s [%(filename)s:%(lineno)s - %(funcName)20s() ] %(levelname)s  %(message)s",
     )
+    if not conf["LOGGER_MODE"]:
+        logging.disable(logging.CRITICAL)
